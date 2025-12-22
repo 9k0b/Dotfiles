b6945b16270958a0297677c9d8c79dae0f421e22
9k0b <jakob.fuenderich@gmail.com>
2025-12-22 14:28:00 +0100

    Delete .config/waybar /Styles directory

--- .config/waybar /Styles/waybar-reload.sh
+++
@@ -1,3 +0,0 @@
-killall -9 waybar
-waybar -c ~/.config/waybar/configuration/waybar_top.jsonc -s ~/.config/waybar/styles/waybar_top_style.css &
-waybar -c ~/.config/waybar/configuration/waybar_right.jsonc -s ~/.config/waybar/styles/waybar_right_style.css &
